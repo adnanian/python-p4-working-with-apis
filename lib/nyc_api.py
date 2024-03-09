1357547b5b93053a37ff5119328186f73c9d9834
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2024-03-08 18:14:58 -0600

    Complete lab with Chicago API instead of New York

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -5,13 +5,25 @@ import json
 class GetPrograms:
 
   def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
-
+    #URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    URL = "https://data.cityofchicago.org/resource/f7f2-ggz5.json"
     response = requests.get(URL)
     return response.content
   
   def program_agencies(self):
-    pass
+    # we use the JSON library to parse the API response into nicely formatted JSON
+        programs_list = []
+        programs = json.loads(self.get_programs())
+        for program in programs:
+            programs_list.append(program["station_name"])
 
-programs = GetPrograms().get_programs()
-print(programs)
+        return programs_list
+
+#programs = GetPrograms().get_programs()
+#print(programs)
+
+programs = GetPrograms()
+agencies = programs.program_agencies()
+
+for agency in set(agencies):
+    print(agency)
